6a91bfe629c69434d102e973f0de84e2b5feca4b
Paulo <pvpmartins@hotmail.com>
2024-01-14 16:34:03 -0300

    added new horizontal scroll hook

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,27 @@ createRoot(root).render(
     <App />
   </Provider>
 )
+
+import { useRef, useEffect } from 'react'
+
+export function useHorizontalScroll(speedFactor = 1) {
+  const elRef = useRef()
+
+  useEffect(() => {
+    const el = elRef.current
+    if (el) {
+      const onWheel = (e) => {
+        if (e.deltaY == 0) return
+        e.preventDefault()
+        el.scrollTo({
+          left: el.scrollLeft - e.deltaY * speedFactor,
+          behavior: 'smooth',
+        })
+      }
+      el.addEventListener('wheel', onWheel)
+      return () => el.removeEventListener('wheel', onWheel)
+    }
+  }, [])
+
+  return elRef
+}
